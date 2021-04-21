1eedbef7e47c3af704944e9cdad91f9f4a979362
Florian Meier <flm0367@thi.de>
2021-04-21 20:47:37 +0200

    Added folder structure and CMake

---
+++ build/CMakeFiles/TicTacToe.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/florian/TicTacToe/src/FieldClass.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/FieldClass.cpp.o"
+  "/home/florian/TicTacToe/src/GameClass.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/GameClass.cpp.o"
+  "/home/florian/TicTacToe/src/InvalidUserInputException.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/InvalidUserInputException.cpp.o"
+  "/home/florian/TicTacToe/src/ParameterOutOfBoundsException.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/ParameterOutOfBoundsException.cpp.o"
+  "/home/florian/TicTacToe/src/PlayerClass.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/PlayerClass.cpp.o"
+  "/home/florian/TicTacToe/src/TileAlreadyOccupiedException.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/TileAlreadyOccupiedException.cpp.o"
+  "/home/florian/TicTacToe/src/main.cpp" "/home/florian/TicTacToe/build/CMakeFiles/TicTacToe.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToe.dir/src/FieldClass.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/GameClass.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/InvalidUserInputException.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/main.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/ParameterOutOfBoundsException.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/PlayerClass.cpp.o"
+  "CMakeFiles/TicTacToe.dir/src/TileAlreadyOccupiedException.cpp.o"
+  "TicTacToe.pdb"
+  "TicTacToe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
