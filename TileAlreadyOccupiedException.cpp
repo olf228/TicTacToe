bba898b5ca0c619bb356bbece1efc9764c393691
Florian <flo-benutzerkonten@posteo.de>
2021-04-21 20:17:49 +0200

    Removed VS Files and added basic folder structure

--- TileAlreadyOccupiedException.cpp
+++
@@ -1,5 +0,0 @@
-#include "TileAlreadyOccupiedException.hpp"
-
-const char* TileAlreadyOccupiedException::what() const noexcept {
-	return "The selected tile is already set. Use other coordinates!";
-}
--- include/ParameterOutOfBoundsException.hpp
+++ include/ParameterOutOfBoundsException.hpp
@@ -1,5 +1,10 @@
+#ifndef _PARAMETEROUTOFBOUNDSEXCEPTION_HPP
+#define _PARAMETEROUTOFBOUNDSEXCEPTION_HPP
+
 #include <stdexcept>
 class ParameterOutOfBoundsException : public std::exception {
 public:
 	virtual const char* what() const noexcept;
 };
+
+#endif
